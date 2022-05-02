0cd3a016e61ea8b9d51ce4206d6548d8db3365f6
allan8araujo <allanzao08@gmail.com>
2022-05-02 14:01:03 -0300

    Updating the BASE_URL and PATH_URL.

--- app/src/main/java/com/example/appfilmecatalogo/utils/Constants.kt
+++ app/src/main/java/com/example/appfilmecatalogo/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.appfilmecatalogo.utils
 
 object Constants {
-    const val BASE_URL ="https://api.themoviedb.org/3/movie/"
-    const val PATH_URL= "popular"
+    const val BASE_URL ="https://api.themoviedb.org/"
+    const val PATH_URL= "3/trending/movie/week"
     }
