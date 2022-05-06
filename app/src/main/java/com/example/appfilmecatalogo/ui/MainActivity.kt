417b13fb11f6340df4c730cb8f4af45dec179a12
allan8araujo@gmail.com <allan8araujo>
2022-05-06 13:31:13 -0300

    Handling some exceptions. Bugs related to exceptions solved.

--- app/src/main/java/com/example/appfilmecatalogo/ui/MainActivity.kt
+++ app/src/main/java/com/example/appfilmecatalogo/ui/MainActivity.kt
@@ -1,30 +1,34 @@
 package com.example.appfilmecatalogo.ui
 
 import android.content.Intent
+import android.graphics.Movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.example.appfilmecatalogo.api.HttpClient
 import com.example.appfilmecatalogo.api.RetrofitService
 import com.example.appfilmecatalogo.databinding.ActivityMainBinding
 import com.example.appfilmecatalogo.models.Lives
+import com.example.appfilmecatalogo.models.MovieResult
 import com.example.appfilmecatalogo.repository.MovieRepository
 import com.example.appfilmecatalogo.utils.Constants
 import com.example.appfilmecatalogo.viewmodel.Movie.MovieViewModel
 import com.example.appfilmecatalogo.viewmodel.Movie.MovieViewModelFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.lang.Exception
+
 
 class MainActivity : AppCompatActivity() {
 
 
     private val retrofitInstanceMain by lazy {
-            Retrofit.Builder()
-                .client(HttpClient.clientInterceptor)
-                .baseUrl(Constants.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+        Retrofit.Builder()
+            .client(HttpClient.clientInterceptor)
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 
     private val movieClient: RetrofitService by lazy {
@@ -56,8 +60,23 @@ class MainActivity : AppCompatActivity() {
 
     private fun getMovieAndObserve() {
         movieViewModel.getAllLives()
-        movieViewModel.movies.observe(this) { Lives ->
-            setListAdapter(Lives)
+        movieViewModel.movies.observe(this) { movieApiResult ->
+            when (movieApiResult) {
+                is MovieResult.Loading -> {
+                    Log.d("Info", "Loading")
+                }
+                is MovieResult.Sucess -> {
+                    Log.d("Info", "Loading")
+                    setListAdapter(movieApiResult.data)
+                }
+                is MovieResult.Error -> {
+                    setListAdapter(movieApiResult.emptyLive)
+                    (Toast.makeText(this,
+                        "Something unexpected happened, try again later.",
+                        Toast.LENGTH_LONG).show())
+                }
+
+            }
         }
     }
 
@@ -67,16 +86,23 @@ class MainActivity : AppCompatActivity() {
 
     private fun goToMovieDetails(movieId: Int) {
         val intent = Intent(this, MovieDetailsActivity::class.java)
-        val movieSelected = movieViewModel.movies.value?.results?.find { PopularWeeklyFilms ->
-            PopularWeeklyFilms.id == movieId
+
+        movieViewModel.movies.observe(this) { movieresult ->
+            when (movieresult) {
+                is MovieResult.Sucess -> {
+                    val movieselected = movieresult.data.results.find { PopularWeeklyFilms ->
+                        PopularWeeklyFilms.id == movieId
+                    }
+                    intent.putExtra("movieId", movieId)
+                    intent.putExtra("movieTitle", movieselected?.title)
+                    intent.putExtra("movieAdult", movieselected?.adult)
+                    intent.putExtra("movieReleaseDate", movieselected?.release_date)
+                    intent.putExtra("movieVoteAverage", movieselected?.vote_average)
+                    intent.putExtra("movieSelected", movieselected)
+                    startActivity(intent)
+                }
+            }
         }
-        intent.putExtra("movieId", movieId)
-        intent.putExtra("movieTitle", movieSelected?.title)
-        intent.putExtra("movieAdult", movieSelected?.adult)
-        intent.putExtra("movieReleaseDate", movieSelected?.release_date)
-        intent.putExtra("movieVoteAverage", movieSelected?.vote_average)
-        intent.putExtra("movieSelected", movieSelected)
-        startActivity(intent)
     }
 
 }
--- app/src/main/java/com/example/appfilmecatalogo/viewmodel/Movie/MovieViewModel.kt
+++ app/src/main/java/com/example/appfilmecatalogo/viewmodel/Movie/MovieViewModel.kt
@@ -2,30 +2,41 @@ package com.example.appfilmecatalogo.viewmodel.Movie
 
 import androidx.lifecycle.*
 import com.example.appfilmecatalogo.models.Lives
+import com.example.appfilmecatalogo.models.MovieResult
+import com.example.appfilmecatalogo.models.mockLives
 import com.example.appfilmecatalogo.models.movieDetails
 import com.example.appfilmecatalogo.repository.IMovieRepository
 import com.example.appfilmecatalogo.repository.IMovieRepositoryDetails
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class MovieViewModel(
-    private val movieRepository: IMovieRepository
+    private val movieRepository: IMovieRepository,
 ) : ViewModel() {
-    private val livelist = MutableLiveData<Lives>()
-    val movies: LiveData<Lives> = livelist
+    private val livelist = MutableLiveData<MovieResult<Lives>>()
+    val movies: LiveData<MovieResult<Lives>> = livelist
 
     fun getAllLives() {
         viewModelScope.launch {
-            val movieFromApi = withContext(Dispatchers.IO) {
-                movieRepository.getAllLives()
+            livelist.value = MovieResult.Loading()
+            try {
+                val movieFromApi = withContext(Dispatchers.IO) {
+                    movieRepository.getAllLives()
+                }
+                livelist.value = MovieResult.Sucess(movieFromApi)
+            } catch (e: Exception) {
+                val movieResult = MovieResult.Error<Lives>(e, mockLives())
+
+                livelist.value=movieResult
             }
-            livelist.value = movieFromApi
         }
     }
 }
 
-class MovieViewModelDetails(private val movieRepositoryDetails: IMovieRepositoryDetails
+class MovieViewModelDetails(
+    private val movieRepositoryDetails: IMovieRepositoryDetails,
 ) : ViewModel() {
     private val detailMovie = MutableLiveData<movieDetails>()
     val movies: LiveData<movieDetails> = detailMovie
